63b1cc804deaeb7c89792ff1cf55bf61bc9abffc
Mikayda Mills <mills.ma12@gmail.com>
2019-06-06 20:01:21 -0400

    directives inbuilt

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-directives',
-  templateUrl: './directives.component.html',
-  styleUrls: ['./directives.component.css']
+  selector: "app-directives",
+  templateUrl: "./directives.component.html",
+  styleUrls: ["./directives.component.css"]
 })
 export class DirectivesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  someRandomVariable = true;
+  data = [{ name: "Mikayda", age: 27 }, { name: "Dallin", age: 26 }];
+  constructor() {
+    setInterval(() => {
+      this.someRandomVariable = !this.someRandomVariable;
+    }, 1000);
   }
 
+  ngOnInit() {}
 }
