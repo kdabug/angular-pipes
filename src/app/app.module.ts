88023e939ccfcf197935405472831727b2c93ab4
Mikayda Mills <mills.ma12@gmail.com>
2019-06-06 20:19:01 -0400

    custom directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from "./app.component";
 import { ReversePipe } from './reverse.pipe';
 import { DirectivesComponent } from './directives/directives.component';
 import { PipesComponent } from './pipes/pipes.component';
+import { RedblackDirective } from './redblack.directive';
 
 @NgModule({
-  declarations: [AppComponent, ReversePipe, DirectivesComponent, PipesComponent],
+  declarations: [AppComponent, ReversePipe, DirectivesComponent, PipesComponent, RedblackDirective],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { RedblackDirective } from "../redblack.directive";
 
 @Component({
   selector: "app-directives",
